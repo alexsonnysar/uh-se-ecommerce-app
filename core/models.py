99eafe0ecc0f46b3d7768c64fb138e651cb593e9
hobryan74 <hobryan74@yahoo.com>
2020-04-16 15:03:58 -0500

    description attribute added

--- core/models.py
+++ core/models.py
@@ -5,6 +5,7 @@ from django.db import models
 class Product(models.Model):
     name = models.CharField(max_length=100)
     price = models.IntegerField(default=1)
+    description = models.TextField(max_length=100, default='')
 
     def __str__(self):
         return self.name
