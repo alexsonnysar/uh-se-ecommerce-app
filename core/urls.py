c9cbc2316746c9d0dca9ec06057d196e78ed9e9b
alexsonnysar <alexsonnysar@gmail.com>
2020-04-19 18:15:28 -0500

    Fix mispelled exists function

--- core/urls.py
+++ core/urls.py
@@ -12,5 +12,5 @@ urlpatterns = [
     path('', HomeView.as_view(), name='home'),
     path('product/<slug>/', ItemDetailView.as_view(), name='product'),
     path('checkout/', checkout, name='checkout'),
-    path('add-to-cart/<slug>', add_to_cart, name='add-to-cart')
+    path('add-to-cart/<slug>/', add_to_cart, name='add-to-cart')
 ]
--- core/views.py
+++ core/views.py
@@ -17,17 +17,15 @@ class ItemDetailView(DetailView):
 def add_to_cart(request, slug):
     item = get_object_or_404(Item, slug=slug)
     order_item = OrderItem.objects.create(item=item)
-    order_qs = Order.objects.filter(user=request.user,is_ordered=False )
-    if order_qs.exist():
+    order_qs = Order.objects.filter(user=request.user, is_ordered=False)
+    if order_qs.exists():
         order = order_qs[0]
         if order.items.filter(items__slug=item.slug).exist():
             order_item.quantity += 1
             order_item.save()
-
     else:
         ordered_date = timezone.now()
-        order = Order.objects.create(user,request.user,ordered_date=ordered_date)
+        order = Order.objects.create(user,request.user, ordered_date=ordered_date)
         order.items.add(order_item)
-    
     return redirect("core:product", slug=slug)
 
