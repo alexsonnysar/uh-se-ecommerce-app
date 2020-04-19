85917bf8a1e46a42ca797262754d344dea0b1552
Alex Paniagua <alexpaniagua@Alexs-MacBook-Pro.local>
2020-04-19 16:43:43 -0500

    Modified MDBootstrap template

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
-from .views import item_list
+from .views import ( 
+    HomeView,
+    ItemDetailView,
+    checkout
+)
 
 app_name = 'core'
 
 urlpatterns = [
-    path('', item_list, name='item-list')
+    path('', HomeView.as_view(), name='home'),
+    path('product/<slug>', ItemDetailView.as_view(), name='product'),
+    path('checkout/', checkout, name='checkout')
 ]
--- core/views.py
+++ core/views.py
@@ -1,8 +1,17 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
 from .models import Item
 
-def item_list(request):
-    context = {
-        'items': Item.objects.all()
-    }
-    return render(request, 'home-page.html', context)
+class HomeView(ListView):
+    model = Item
+    template_name = 'home-page.html'
+
+def checkout(request):
+    return render(request, 'checkout-page.html')
+
+
+class ItemDetailView(DetailView):
+    model = Item
+    template_name = 'product-page.html'
+
+
