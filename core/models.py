5cd3b2cfef032133392ec6196e7a8a9f52e80343
apani47135 <apani47135@gmail.com>
2020-04-19 17:25:38 -0500

    Fix url link to product page

--- core/models.py
+++ core/models.py
@@ -1,12 +1,19 @@
 from django.conf import settings
 from django.db import models
+from django.shortcuts import reverse
 
 class Item(models.Model):
     name = models.CharField(max_length=100)
     price = models.FloatField()
+    slug = models.SlugField()
 
     def __str__(self):
         return self.name
+    def get_absolute_url(self):
+        return reverse("core:product", kwargs ={
+            'slug': self.slug
+        })
+
 
 class OrderItem(models.Model):
     item = models.ForeignKey(Item, on_delete=models.CASCADE)
--- core/urls.py
+++ core/urls.py
@@ -9,6 +9,6 @@ app_name = 'core'
 
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
-    path('product/<slug>', ItemDetailView.as_view(), name='product'),
+    path('product/<slug>/', ItemDetailView.as_view(), name='product'),
     path('checkout/', checkout, name='checkout')
 ]
