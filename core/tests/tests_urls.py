112f377fb644fa1be9798f6e44bec958ead36514
alexsonnysar <alexsonnysar@gmail.com>
2020-04-23 16:56:08 -0500

    Added tests for 3 urls with bjho

--- core/tests/tests_urls.py
+++ core/tests/tests_urls.py
@@ -1,8 +1,28 @@
 from django.test import SimpleTestCase
 from django.urls import reverse, resolve
-from core.views import HomeView
+from core.views import (
+    HomeView,
+    ItemDetailView,
+    CheckoutView,
+    add_to_cart,
+    remove_item_from_cart,
+    CartView,
+    item_quantity_reduction,
+    OrderConfirmationView,
+    MyOrdersViews,
+)
 
 class TestUrls(SimpleTestCase):
-    def test_home_url_is_resolved(self):
-        url = reverse('home')
+    def test_home_url_resolves(self):
+        url = reverse('core:home')
         self.assertEquals(resolve(url).func.view_class, HomeView)
+
+    def test_product_url_resolves(self):
+        url = reverse('core:product', args=['some-slug'])
+        self.assertEqual(resolve(url).func.view_class, ItemDetailView)
+
+    def test_checkout_url_reseolves(self):
+        url = reverse('core:checkout')
+        self.assertEquals(resolve(url).func.view_class, CheckoutView)
+    
+    
