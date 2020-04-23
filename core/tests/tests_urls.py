ca5914e99e5b2a82c6f7ae0a991aaaac3127d3ec
hobryan74 <hobryan74@yahoo.com>
2020-04-23 17:06:09 -0500

    url test complete

--- core/tests/tests_urls.py
+++ core/tests/tests_urls.py
@@ -12,17 +12,40 @@ from core.views import (
     MyOrdersViews,
 )
 
+
 class TestUrls(SimpleTestCase):
     def test_home_url_resolves(self):
-        url = reverse('core:home')
+        url = reverse("core:home")
         self.assertEquals(resolve(url).func.view_class, HomeView)
 
     def test_product_url_resolves(self):
-        url = reverse('core:product', args=['some-slug'])
+        url = reverse("core:product", args=["some-slug"])
         self.assertEqual(resolve(url).func.view_class, ItemDetailView)
 
     def test_checkout_url_reseolves(self):
-        url = reverse('core:checkout')
+        url = reverse("core:checkout")
         self.assertEquals(resolve(url).func.view_class, CheckoutView)
-    
-    
+
+    def test_add_to_cart_url(self):
+        url = reverse("core:add-to-cart", args=["some slug"])
+        self.assertEquals(resolve(url).func, add_to_cart)
+
+    def test_remove_item_from_cart_url(self):
+        url = reverse("core:remove-item-from-cart", args=["some-slug"])
+        self.assertEqual(resolve(url).func, remove_item_from_cart)
+
+    def test_cart_view_url(self):
+        url = reverse("core:cart")
+        self.assertEquals(resolve(url).func.view_class, CartView)
+
+    def test_item_quantity_reduction_url(self):
+        url = reverse("core:item-quantity-reduction", args=["some=slug"])
+        self.assertEquals(resolve(url).func, item_quantity_reduction)
+
+    def test_order_confirmation_url(self):
+        url = reverse("core:order-confirmation")
+        self.assertEqual(resolve(url).func.view_class, OrderConfirmationView)
+
+    def test_my_order_url(self):
+        url = reverse("core:history")
+        self.assertEquals(resolve(url).func.view_class, MyOrdersViews)
