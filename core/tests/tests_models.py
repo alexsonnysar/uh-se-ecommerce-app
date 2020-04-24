1ad9f656059cfb3cb68d765474463cb25e1e14b9
hobryan74 <hobryan74@yahoo.com>
2020-04-24 17:52:44 -0500

    test model update

--- core/tests/tests_models.py
+++ core/tests/tests_models.py
@@ -1,4 +1,5 @@
 from django.test import TestCase
+from allauth.utils import get_user_model
 from core.models import (
     Item,
     OrderItem,
@@ -10,11 +11,24 @@ from core.models import (
 class TestModels(TestCase):
     def setUp(self):
         self.beef = Item.objects.create(
-            name="beef", price=5, slug="beef", picture="some-link", description="Texas Angus Beef",
+            name="beef",
+            price=5,
+            slug="beef",
+            picture="some-link",
+            description="Texas Angus Beef",
         )
-        # self.order_item = OrderItem.objects.create(
-
-        # )
+        self.order_item = OrderItem.objects.create(
+            user=get_user_model().objects.create(username="@raymond.penners"),
+            item=self.beef,
+            quantity=4,
+            is_ordered=False,
+        )
+        self.order_item = OrderItem.objects.create(
+            user=get_user_model().objects.create(username="@raymond.penners"),
+            item=self.beef,
+            quantity=4,
+            is_ordered=False,
+        )
 
     def test_item_is_assigned_attributes_on_creation(self):
         self.assertEquals(self.beef.name, "beef")
@@ -23,4 +37,7 @@ class TestModels(TestCase):
         self.assertEquals(self.beef.description, "Texas Angus Beef")
 
     def test_order_item_is_assigned_attributes_on_creation(self):
-        pass
+        self.assertEquals(self.order_item.user.username, "@raymond.penners")
+        self.assertEquals(self.order_item.item, self.beef)
+        self.assertEquals(self.order_item.quantity, 4)
+        self.assertEquals(self.order_item.is_ordered, False)
