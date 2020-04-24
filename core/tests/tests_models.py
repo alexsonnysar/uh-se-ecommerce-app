0be461adf8c5b62e46cb18406cb9f19d7823a08a
alexsonnysar <alexsonnysar@gmail.com>
2020-04-24 17:24:51 -0500

    Started mock order item object

--- core/tests/tests_models.py
+++ core/tests/tests_models.py
@@ -10,8 +10,17 @@ from core.models import (
 class TestModels(TestCase):
     def setUp(self):
         self.beef = Item.objects.create(
-            name="beef", price=5, slug="beef", description="Texas Angus Beef",
+            name="beef", price=5, slug="beef", picture="some-link", description="Texas Angus Beef",
         )
+        # self.order_item = OrderItem.objects.create(
 
-    def test_item_is_assigned_slug_on_creation(self):
-        self.assertEquals(self.beef.slug, "beef")
+        # )
+
+    def test_item_is_assigned_attributes_on_creation(self):
+        self.assertEquals(self.beef.name, "beef")
+        self.assertEquals(self.beef.price, 5)
+        self.assertEquals(self.beef.picture, "some-link")
+        self.assertEquals(self.beef.description, "Texas Angus Beef")
+
+    def test_order_item_is_assigned_attributes_on_creation(self):
+        pass
